c8ae8bb4022aa9b16cb672557aa387112d2bbd06
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2022-10-11 11:12:04 -0300

    Sorteio-String

---
+++ Exercicios em c/Sorteio_nomes.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+void Quantidade(int *tam)
+{
+    printf("Digite a quantidade de nomes: ");
+    scanf("%d", tam);
+    getchar();
+}
+
+void Gravar(char **nomes, int *tam)
+{
+    int i;
+    printf("Digite %d nomes.\n\n", *tam);
+    for (i = 0; i < *tam; i++)
+    {
+        nomes[i] = calloc(20, sizeof(char));
+    }
+
+    for (i = 0; i < *tam; i++)
+    {
+        printf("%d. - ", i + 1);
+        fgets(nomes[i], 20, stdin);
+    }
+}
+
+void Sorteio(char **nomes, int *tam)
+{
+    int sort;
+    srand(time(NULL));
+
+    sort = rand() % *tam;
+    printf("%d", sort);
+    printf("\n\nO nome sorteado foi: %s\n", &nomes[sort][0]);
+}
+
+void Menu()
+{
+    printf("Opcao\n1 - Sortear.\n2 - Sair.");
+}
+
+int main()
+{
+    int op;
+
+    int *tam = malloc(sizeof(int));
+    char **nomes = calloc(3, sizeof(char *));
+    Quantidade(tam);
+    Gravar(nomes, tam);
+    
+    for (;;)
+    {
+        Menu();
+        scanf("%d", &op);
+        switch (op)
+        {
+        case 1:
+            Sorteio(nomes, tam);
+            break;
+        case 2:
+            puts("\n\nSaindo...\n");
+            exit(0);
+        default:
+            puts("opcao invalida.\n");
+        }
+    }
+
+    return 0;
+}
